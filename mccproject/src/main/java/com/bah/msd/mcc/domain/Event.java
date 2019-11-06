da78e98edfadccd858f36b5035e6f33ab68ea38c
ellard.x.lamb <lamb_ellard@bah.com>
2019-11-06 16:37:40 -0500

    Edit data.sql to include insert for Events table, added Event class, added EventRepository interface

---
+++ mccproject/src/main/java/com/bah/msd/mcc/domain/Event.java
@@ -0,0 +1,49 @@
+package com.bah.msd.mcc.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EVENTS")
+public class Event {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="EVENT_CODE")
+	Long code;
+	@Column(name="EVENT_TITLE")
+	String title;
+	@Column(name="EVENT_DESCRIPTION")
+	String description;
+	
+	public Event() {
+		super();
+	}
+
+	public Long getCode() {
+		return code;
+	}
+
+	public void setCode(Long code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
--- mccproject/src/main/resources/data.sql
+++ mccproject/src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Bruce', 'bruce@a.com', 'changeit');
 insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Paul', 'paul@b.com', 'changeit');
 insert into CUSTOMERS (CUSTOMER_NAME, EMAIL, PASSWORD) VALUES ('Rick', 'rick@c.com', 'changeit');
+
+-- insert into EVENTS (EVENT_CODE, EVENT_TITLE, EVENT_DESCRIPTION) VALUES ('001', 'MSD Bootcamp', 'Kickoff for Java Modern Software Development bootcamp');
