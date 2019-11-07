7c0ad14e596430971b3b7201129e3228f3c1c62d
user <mijungita@gmail.com>
2019-11-07 16:01:50 -0500

    Added Registration and RegistrationRepository

---
+++ mccproject/src/main/java/com/bah/msd/mcc/domain/Registration.java
@@ -0,0 +1,69 @@
+package com.bah.msd.mcc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Registration {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String eventName;
+	@Column
+	private String customerName;
+	@Column
+	private Date date;
+	@Column
+	private String note;
+	
+	public Registration() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}	
+}
